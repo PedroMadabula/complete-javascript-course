03a85a6ae51b711277127aa125c47791fb047f71
PedroMadabula <56992852+PedroMadabula@users.noreply.github.com>
2020-07-18 04:00:47 -0700

    Adding a New Item to Our Budget Controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -27,6 +27,37 @@ var budgetController = (function() {
             inc: 0
         }
     }
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem;
+            ID = 0;
+
+            //ID = last ID + 1
+
+            //Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length-1].id + 1;
+            }
+            else {
+                ID = 0;
+            }
+            //Create new item based on 'inc' or 'exp' type
+            if (type === "exp") {
+                newItem = new Expense(ID, des, val);    
+            }
+            else if (type === "inc") {
+                newItem = new Income(ID, des, val);
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+        
+        testing: function() {
+            console.log(data);
+        }
+    }
 })();
 
 //UI Controller
@@ -61,7 +92,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var DOM = UICtrl.getDOMstrings();
         document.querySelector(DOM.inputButton).addEventListener("click", crtlAddItem);
     
-        document.addEventListener("keypre  ss", function(event) { 
+        document.addEventListener("keypress", function(event) { 
             if (event.keyCode === 13 || event.which === 13) {
                 crtlAddItem();
             }
@@ -69,9 +100,12 @@ var controller = (function(budgetCtrl, UICtrl) {
     }
     
     var crtlAddItem = function() {
+
+        var input, newItem;
         //1. Get the field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
         //2. Add the item to the buget controller
+        budgetCtrl.addItem(input.type, input.description, input.value);
         //3. Add the item to the UI
         //4. Calculate the budget
         //5. Display the budget on the UI
