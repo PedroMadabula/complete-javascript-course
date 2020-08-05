3c01c8c268fe5b45fe710aa0d2d43228dbd79b61
PedroMadabula <56992852+PedroMadabula@users.noreply.github.com>
2020-08-05 00:09:44 -0700

    Variable Declarations with let and const

---
+++ 7-ES6/starter/script.js
@@ -0,0 +1,42 @@
+/*
+//Lecture: let and const
+
+// ES5
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// ES6
+const name6 = 'Jane Smith'; //constant
+let age6 = 23; //can change
+
+name6 = 'Jane Miller';
+console.log(name6);
+*/
+
+// ES5
+function driversLicense5(passedTest) {
+
+    if (passedTest) {
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+    console.log(firstName + ", born in " + yearOfBirth + ", is now officially allowed to drive a car.");
+}
+
+driversLicense5(true);
+
+//ES6
+function driversLicense6(passedTest) {
+
+    let firstName;
+    const yearOfBirth = 1990;
+
+    if (passedTest) {
+        firstName = 'John';
+    }
+    console.log(firstName + ", born in " + yearOfBirth + ", is now officially allowed to drive a car.");
+}
+
+driversLicense6(true);
